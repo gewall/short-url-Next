8f92df62d863b8797e6914e0594be99f7b32d33f
Gewall <alginugraha21@gmail.com>
2026-04-18 14:24:56 +0700

    add api status information

--- components/LPNavbar.tsx
+++ components/LPNavbar.tsx
@@ -1,17 +1,44 @@
+"use client"
+
 import { LogIn } from "lucide-react"
 import Container from "./Container"
 import Logo from "./Logo"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { useEffect, useState } from "react"
+import { Spinner } from "./ui/spinner"
 
 export default function LPNavbar() {
+  const [apiStatus, setApiStatus] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}health`)
+        if (req.ok) {
+          setApiStatus(true)
+        } else {
+          setApiStatus(false)
+        }
+        setLoading(false)
+      } catch {
+        setApiStatus(false)
+        setLoading(false)
+      }
+    })()
+  }, [])
   return (
     <nav className="">
       <Container className="flex items-center justify-between">
         <div>
           <Logo />
         </div>
-        <div>
+        <div className="flex items-center gap-2">
+          {loading && <Spinner className="text-primary" />}
+          {!apiStatus && !loading && (
+            <span className="text-sm text-destructive">API is down</span>
+          )}
           <Button variant="outline" asChild>
             <Link href="/login">
               <LogIn className="text-primary" />
