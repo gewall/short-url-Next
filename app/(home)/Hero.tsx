b39335e985ba2dc2a90c5c111f6f57ff3aead30c
Gewall <alginugraha21@gmail.com>
2026-04-18 14:56:01 +0700

    remove /

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -97,7 +97,8 @@ export default function Hero({ content }: HeroProps) {
             </form>
             <p className="mt-2 text-sm text-muted-foreground">Result :</p>
             <div className="rounded-md bg-accent p-4">
-              {process.env.NEXT_PUBLIC_API_URL}/{code}
+              {process.env.NEXT_PUBLIC_API_URL}
+              {code}
             </div>
           </CardContent>
         </Card>
